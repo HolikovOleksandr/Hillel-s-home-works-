7ae9d76819dcb97f8cb1aa5d35987f58f67124e1
Holikov_Okesandr <santagolikov97@gmail.com>
2023-02-16 13:58:04 +0300

    Change HW3IfElse

--- HW3IfElse/Program.cs
+++ HW3IfElse/Program.cs
@@ -4,94 +4,36 @@ namespace IfElse
     {
         static void Main(string[] args)
         {
-            // IF/ELSE
-            string _positiveAnswer = "Successful!";
-            string _negativeAnswer = "Misstake!";
-            string _finalAnswer;
-            string? _login;
-            bool result;
+            string _incorrectPassword = "Password is not correct";
+            string _incorrectLogin = "Login is not correct";
+            string _finalAnswer = "";
+            string _login;
 
-
-            // Story 1
-            _positiveAnswer = "Hello User";
-
-            Console.WriteLine("Story 1");
-            Console.WriteLine("Enter login: ");
+            Console.WriteLine("Hi. Pleace, enter login and press ENTER: ");
             _login = Console.ReadLine().ToUpper();
 
-            if (_login == "USER")
+            if (_login == "") _finalAnswer = "Hello Anonimous";
+            else if (_login == "USER" || _login == "ADMIN")
             {
-                result = CreateAndConfirmPassword();
+                Console.WriteLine("Enter password and press ENTER: ");
+                string? password = Console.ReadLine();
 
-                if (result) _finalAnswer = _positiveAnswer;
-                else _finalAnswer = _negativeAnswer;
+                Console.WriteLine("Confirm password and press ENTER: ");
+                string? confirmPassword = Console.ReadLine();
 
-                Console.WriteLine(_finalAnswer);
+                if (password == confirmPassword)
+                {
+                    if (_login == "USER") _finalAnswer = "Hello User";
+                    else if (_login == "ADMIN") _finalAnswer = "Hello Administrator";
+                }
+                else _finalAnswer = _incorrectPassword;
             }
-            else Console.WriteLine(_negativeAnswer);
-
-
-            // Story 2
-            _positiveAnswer = "Hello Administrator";
-
-            Console.WriteLine("Story 2");
-            Console.WriteLine("Enter login: ");
-            _login = Console.ReadLine().ToUpper();
-
-            if (_login == "ADMIN")
-            {
-                result = CreateAndConfirmPassword();
-
-                if (result) _finalAnswer = _positiveAnswer;
-                else _finalAnswer = _negativeAnswer;
-
-                Console.WriteLine(_finalAnswer);
-            }
-            else Console.WriteLine(_negativeAnswer);
-
-
-            // story 3
-            _positiveAnswer = "Hello Anonimous";
-
-            Console.WriteLine("story 3");
-            Console.WriteLine("enter login: ");
-            _login = Console.ReadLine();
-
-            if (_login == "") _finalAnswer = _positiveAnswer;
-            else _finalAnswer = _negativeAnswer;
+            else _finalAnswer = _incorrectLogin;
 
             Console.WriteLine(_finalAnswer);
 
-
-            // Story 4
-            _negativeAnswer = "Password is not correct";
-
-            Console.WriteLine("Story 4");
-            Console.WriteLine("Enter login: ");
-            _login = Console.ReadLine().ToUpper();
-
-            if (_login == "ADMIN" || _login == "USER")
-            {
-                result = CreateAndConfirmPassword();
-
-                if (result) _finalAnswer = _positiveAnswer;
-                else _finalAnswer = _negativeAnswer;
-
-                Console.WriteLine(_finalAnswer);
-            }
-            else Console.WriteLine(_negativeAnswer);
-        }
-
-        static bool CreateAndConfirmPassword()
-        {
-            Console.WriteLine("Enter password: ");
-            string? password = Console.ReadLine();
-
-            Console.WriteLine("Confirm password: ");
-            string? confirmPassword = Console.ReadLine();
-
-            bool result = password == confirmPassword ? true : false;
-            return result;
+            Console.WriteLine("Press any button to exit the program");
+            Console.ReadLine();
         }
     }
 }
---
+++ HW5Cycles/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        // Console.WriteLine("Write symbol and press ENTER: ");
+        // char _symbol = char.Parse(Console.ReadLine());
+        char _symbol = '*';
+
+        // Console.WriteLine("Write stroke count and press ENTER: ");
+        // int _strokeCount = int.Parse(Console.ReadLine());
+        int _strokeCount = 5;
+
+
+        for (int i = 0; i < _strokeCount; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write(_symbol);
+                
+                while (j == _strokeCount)
+                {
+                    for (int k = _strokeCount - 1; k > 0; k--) Console.Write(_symbol);
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine();
+
+        }
+
+        Console.WriteLine("Press any key for leave from program");
+        Console.Read();
+    }
+}
