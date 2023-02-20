9178cecc4b70da0584aed2c42cf293bb8c697f39
Holikov_Okesandr <santagolikov97@gmail.com>
2023-02-20 15:43:22 +0200

    Fixed HW2

--- HW2Variables/Program.cs
+++ HW2Variables/Program.cs
@@ -1,7 +1,7 @@
 ﻿public class Program
 {
     private static void Main(string[] args)
-    {
+    {   
         byte humanAge = 42;
         double bankAccount = 123456654321;
         char letter = 'А';
@@ -9,16 +9,16 @@
         bool personLiveThousandYears = false;
         float humanTemperature = 36.6f;
         ulong earthPopulation = 7942645086;
-        sbyte ukrainianAmbiendTemperature = -1;
+        float ukrainianAmbiendTemperature = -1.75f;
 
-        Console.WriteLine("Вiк людини: " + humanAge);
-        Console.WriteLine("Банкiвський рахунок: " + bankAccount);
-        Console.WriteLine("Лiтера: " + letter);
-        Console.WriteLine("SMS повiдомлення: " + massageSMS);
-        Console.WriteLine("Чи може людина жити 1000 рокiв: " + personLiveThousandYears);
-        Console.WriteLine("Температура людини: " + humanTemperature);
-        Console.WriteLine("Населення планети: " + earthPopulation);
-        Console.WriteLine("Температура навколишньго середовища: " + ukrainianAmbiendTemperature);
+        Console.WriteLine($"Вiк людини: {humanAge}");
+        Console.WriteLine($"Банкiвський рахунок: {bankAccount}");
+        Console.WriteLine($"Лiтера: {letter}");
+        Console.WriteLine($"SMS повiдомлення: {massageSMS}");
+        Console.WriteLine($"Чи може людина жити 1000 рокiв: {personLiveThousandYears}");
+        Console.WriteLine($"Температура людини: {humanTemperature}");
+        Console.WriteLine($"Населення планети: {earthPopulation}");
+        Console.WriteLine($"Температура навколишньго середовища: {ukrainianAmbiendTemperature} за Цельсієм");
     }
 }
 
