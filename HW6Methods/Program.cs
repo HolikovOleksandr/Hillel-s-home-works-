7e1b008ff6e33816a88073d0cccbfb8a59b15b9b
Holikov_Okesandr <santagolikov97@gmail.com>
2023-02-21 23:01:19 +0200

    Upgrade HW 6

--- HW6Methods/Program.cs
+++ HW6Methods/Program.cs
@@ -6,70 +6,34 @@
         string? firstInput = Console.ReadLine();
         int firstValue = int.Parse(firstInput);
 
-        Console.WriteLine("Hello. Please enter the second integer: ");
+        Console.WriteLine("And second integer: ");
         string? secondInput = Console.ReadLine();
         int secondValue = int.Parse(secondInput);
 
         Console.WriteLine("What we wil do with those integers?");
         Console.WriteLine("Press +, -, *, / or ^");
         char @operator = char.Parse(Console.ReadLine());
-        
-        if (@operator == '+'
-            || @operator == '-'
-            || @operator == '*'
-            || @operator == '/'
-            || @operator == '^')
-        {
 
-            switch (@operator)
-            {
-                case '+':
-                    {
-                        Console.WriteLine(Addition(firstValue, secondValue));
-                        break;
-                    }
-                case '-':
-                    {
-                        Console.WriteLine(Subtraction(firstValue, secondValue));
-                        break;
-                    }
-                case '*':
-                    {
-                        Console.WriteLine(Multiplication(firstValue, secondValue));
-                        break;
-                    }
-                case '/':
-                    {
-                        if (secondValue == 0) Console.WriteLine("I can`t divide by zero");
-                        else Console.WriteLine(Division(firstValue, secondValue));
-                        break;
-                    }
-                case '^':
-                    {
-                        Console.WriteLine(Exponentiation(firstValue, secondValue));
-                        break;
-                    }
-            }
+        int answer = 0;
+        if (@operator == '+') answer = Addition(firstValue, secondValue);
+        if (@operator == '-') answer = Subtraction(firstValue, secondValue);
+        if (@operator == '*') answer = Multiplication(firstValue, secondValue);
+        if (@operator == '^') answer = Exponentiation(firstValue, secondValue);
+        if (@operator == '/')
+        {
+            if (secondValue == 0) Console.WriteLine("I can`t divide by zero");
+            else answer = Division(firstValue, secondValue);
         }
-        else Console.WriteLine("I don't know this operation");
+        Console.WriteLine($"Answer is: {answer}");
 
         Console.WriteLine("Press any button for close program");
         Console.ReadKey();
     }
 
-    // Case +
     static int Addition(int firstValue, int secondValue) => firstValue + secondValue;
-
-    // Case -
     static int Subtraction(int firstValue, int secondValue) => firstValue - secondValue;
-
-    // Case *
     static int Multiplication(int firstValue, int secondValue) => firstValue * secondValue;
-
-    // Case /
     static int Division(int firstValue, int secondValue) => firstValue / secondValue;
-
-    //Case ^
     static int Exponentiation(int firstValue, int secondValue)
     {
         int result = 1;
