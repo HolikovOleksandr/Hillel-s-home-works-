f2d156f80573be5d456cba239a150840f4d1d929
Oleksandr Holikov <93937129+HolikovOleksandr@users.noreply.github.com>
2023-02-26 19:44:09 +0200

    HW7 Done

---
+++ HW7Overload/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(Add(7, 3)); // Int
+        Console.WriteLine(Add(6.5, 3.5)); // Double
+        Console.WriteLine(Add("6.5", "3.5")); // String
+
+        Console.WriteLine("Press any key to exit the program");
+        Console.ReadKey();
+    }
+
+    static int Add(int a, int b) => a + b; // Int
+    static double Add(double a, double b) => a + b; // Double
+    static string Add(string a, string b) // String
+    {
+        bool successfulParce;
+        double aDouble;
+        double bDouble;
+        double result;
+
+        successfulParce = double.TryParse(a, out aDouble);
+        successfulParce = double.TryParse(b, out bDouble);
+
+        result = aDouble + bDouble;
+        return result.ToString();
+    }
+}
---
+++ HW7Recursion/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    static int n;
+
+    private static void Main(string[] args)
+    {
+
+        Console.WriteLine("Enter the number: ");
+        n = int.Parse(Console.ReadLine());
+
+        string result;
+        if (n <= -1) result = "Неприпустиме значення аргументу";
+        else result = SumTo(n).ToString();
+
+        Console.WriteLine(result);
+
+        Console.WriteLine("Press any key to exit the program");
+        Console.ReadKey();
+    }
+
+    static int SumTo(int n)
+    {
+        return n = (n == 0 || n == 1) ? 1 : n + SumTo(n - 1);
+    }
+}
