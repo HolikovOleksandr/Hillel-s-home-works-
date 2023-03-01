b9fafd5e6ecdc039587e9d17f5a168c3c693e550
Oleksandr Holikov <93937129+HolikovOleksandr@users.noreply.github.com>
2023-03-01 22:43:55 +0200

    HW8 Done

--- HW5Cycles/Program.cs
+++ HW5Cycles/Program.cs
@@ -11,16 +11,16 @@
         for (int i = 1; i <= _number; i++)
         {
             for (int j = 1; j <= _number - i; j++) Console.Write(" ");
-            for (int j = 1; j <= 2 * i - 1; j++) Console.Write("*");
+            for (int j = 1; j <= 2 * i - 1; j++) Console.Write(_symbol);
             Console.WriteLine();
         }
 
-        for (int i = _number - 1; i >= 1; i--)
-        {
-            for (int j = 1; j <= _number - i; j++) Console.Write(" ");
-            for (int j = 1; j <= 2 * i - 1; j++) Console.Write("*");
-            Console.WriteLine();
-        }
+        // for (int i = _number - 1; i >= 1; i--)
+        // {
+        //     for (int j = 1; j <= _number - i; j++) Console.Write(" ");
+        //     for (int j = 1; j <= 2 * i - 1; j++) Console.Write(_symbol);
+        //     Console.WriteLine();
+        // }
 
         Console.WriteLine("Press any key for leave from program");
         Console.Read();
---
+++ HW8Arrays/Program.cs
@@ -0,0 +1,58 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.WriteLine("Hello! Write symbol and press ENTER: ");
+        char symbol = char.Parse(Console.ReadLine());
+
+        Console.WriteLine("Hello! Write ONLY odd number and press ENTER: ");
+        int triangleWidth = int.Parse(Console.ReadLine());
+
+        char[,] array = DrawTriangleInDoubleArray(triangleWidth, symbol);
+        PrintArrayInConsole(array);
+
+        Console.WriteLine("Press any key for leave from program");
+        Console.Read();
+    }
+
+    /// <summary>
+    /// Create char[,] array and saved them triangle of symbols;
+    /// </summary>
+    /// <param name="triangleWidth">Conut of symbols in triangle last row</param>
+    /// <param name="symbol">Symbol for drawing triangle</param>
+    /// <returns>char[,] array</returns>
+    static char[,] DrawTriangleInDoubleArray(int triangleWidth, char symbol)
+    {
+        int size;
+        if (triangleWidth % 2 == 1) size = triangleWidth;
+        else size = triangleWidth++;
+
+        char[,] array = new char[size, size];
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                array[i, j] = ' ';
+
+        int center = size / 2;
+        for (int i = 0; i <= center; i++)
+            for (int j = center - i; j <= center + i; j++)
+                array[i, j] = symbol;
+
+        return array;
+    }
+
+    /// <summary>
+    /// Function writed in console char`s double array
+    /// </summary>
+    /// <param name="array">Array to be drawn</param>
+    static void PrintArrayInConsole(char[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+                Console.Write(array[i, j]);
+
+            Console.WriteLine();
+        }
+    }
+}
