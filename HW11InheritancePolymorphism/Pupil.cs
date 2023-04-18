a488f7929a79ade6128ce1194d140441f3dd3665
Oleksandr <sanyagolikov97@gmail.com>
2023-04-18 16:50:50 +0300

    Test commit

--- HW11InheritancePolymorphism/Pupil.cs
+++ HW11InheritancePolymorphism/Pupil.cs
@@ -1,10 +1,7 @@
 public class Pupil
 {
     public virtual void Studi() { Console.WriteLine($"I can {nameof(Studi)}."); }  
-
     public virtual void Read() { Console.WriteLine($"I can {nameof(Read)}."); } 
-
     public virtual void Write() { Console.WriteLine($"I can {nameof(Write)}."); }
-        
     public virtual void Relax() { Console.WriteLine($"I can {nameof(Relax)}."); }    
 }
